07b62b28bd9b2aa86085d978a415459eeb3df6a7
seojaeoh <seojaeohcode@gmail.com>
2022-09-22 18:31:27 +0900

    commit

--- stock-server/server.js
+++ stock-server/server.js
@@ -1,3 +1,21 @@
+/* 
+웹스크래핑(현재 주식정보) = axios
+
+웹스크래핑(주간 주식정보) = nightmare
+goto() = 특정 url에 들어감
+evaluate() = 필요한 정보(document.body.innerHTML)을 가져옴.
+cheerio.load = 가져온 정보를 DOM수정하듯 find/text함수를 통해 수정
+
+
+*cors(cross origin resource sharing)
+추가 HTTP헤더를 사용해 
+한 출처에서 실행중인 웹 애플리케이션이 다른 출처의 선택한 자원에 접근권한을 부여하도록 
+브라우저에서 알려주는 시스템
+웹 애플리케이션 => 리소스 => 자신의 출처(도메인, 프로토콜, 포트)와 다를 때 HTTP요청 실행
+
+프론트엔드 서버:8080 vs 서버: 12010 
+=>CORS이슈 =>cors설정
+*/
 const express = require('express')
 const app = express()
 const axios = require('axios')
